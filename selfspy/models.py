0805bdd25bd28ad7683d03bc29451202c17a6eb8
Conor <cjhfitz@gmail.com>
2017-05-04 20:29:33 -0400

    added code to def to_humanreadable, added spaces after the character returns.  Instead of output being like this \t\t\thello it will now appear like this \t\t\t hello

--- selfspy/models.py
+++ selfspy/models.py
@@ -201,19 +201,24 @@ class Keys(SpookMixin, Base):
         return json.loads(zlib.decompress(keys))
 
     def to_humanreadable(self, text):
-        backspace_rex = re.compile("\<\[Backspace\]x?(\d+)?\>", re.IGNORECASE) # creates regualer expression object
+        backspace_rex = re.compile("\<\[Backspace\]x?(\d+)?\>", re.IGNORECASE)  # creates regualer expression object
         tab_rex = re.compile("\<\[Tab\]x?(\d+)?\>", re.IGNORECASE)
         return_rex = re.compile("\<\[Return\]x?(\d+)?\>", re.IGNORECASE)
+
         up_rex = re.compile("\<\[Up\]x?(\d+)?\>", re.IGNORECASE)
         down_rex = re.compile("\<\[Down\]x?(\d+)?\>", re.IGNORECASE)
         left_rex = re.compile("\<\[Left\]x?(\d+)?\>", re.IGNORECASE)
+        right_rex = re.compile("\<\[Right\]x?(\d+)?\>", re.IGNORECASE)
+
 
         backspace_matches = backspace_rex.search(text)
         tab_match = tab_rex.search(text)
         return_match = return_rex.search(text)
+
         up_match = up_rex.search(text)
         down_match = down_rex.search(text)
         left_match = left_rex.search(text)
+        right_match = right_rex.search(text)
 
         while backspace_matches is not None:
             backspaces = backspace_matches.group(1)
@@ -232,9 +237,9 @@ class Keys(SpookMixin, Base):
         while tab_match is not None:
 
             try:
-                tabs = r"\\t" * int(tab_match.group(1))
+                tabs = r"\\t" * int(tab_match.group(1)) + " "
             except TypeError:
-                tabs = r"\\t" # if it fails just place one \t
+                tabs = r"\\t" + " "
 
             text = re.sub("\<\[Tab\]x?(\d+)?\>", tabs, text, 1)
             tab_match = tab_rex.search(text)
@@ -242,9 +247,9 @@ class Keys(SpookMixin, Base):
         while return_match is not None:
 
             try:
-                returns = r"\\r" * int(return_match.group(1))
+                returns = r"\\r" * int(return_match.group(1)) + " "
             except TypeError:
-                returns = r"\\r"
+                returns = r"\\r" + " "
 
             text = re.sub("\<\[Return\]x?(\d+)?\>", returns, text, 1)
             return_match = return_rex.search(text)
@@ -252,9 +257,9 @@ class Keys(SpookMixin, Base):
         while up_match is not None:
 
             try:
-                up_presses = "\^" * int(up_match.group(1))
+                up_presses = "\u" * int(up_match.group(1)) + " "
             except TypeError:
-                up_presses = "\^"
+                up_presses = "\u" + " "
 
             text = re.sub("\<\[Up\]x?(\d+)?\>", up_presses, text, 1)
             up_match = up_rex.search(text)
@@ -262,9 +267,9 @@ class Keys(SpookMixin, Base):
         while down_match is not None:
 
             try:
-                down_button_presses = "\down" * int(down_match.group(1))
+                down_button_presses = "\d" * int(down_match.group(1)) + " "
             except TypeError:
-                down_button_presses = "\down"
+                down_button_presses = "\d" + " "
 
             text = re.sub("\<\[Down\]x?(\d+)?\>", down_button_presses, text, 1)
             down_match = down_rex.search(text)
@@ -272,14 +277,25 @@ class Keys(SpookMixin, Base):
         while left_match is not None:
 
             try:
-                left_button_presses = "\left" * int(left_match.group(1))
+                left_button_presses = "\l" * int(left_match.group(1)) + " "
             except TypeError:
-                left_button_presses = "\left"
+                left_button_presses = "\l" + " "
 
             text = re.sub("\<\[Left\]x?(\d+)?\>", left_button_presses, text, 1)
             left_match = left_rex.search(text)
 
+        while right_match is not None:
+
+            try:
+                right_button_presses = r"\ri" * int(right_match.group(1)) + " "
+            except TypeError:
+                right_button_presses = r"\ri" + " "
+
+            text = re.sub("\<\[Right\]x?(\d+)?\>", right_button_presses, text, 1)
+            right_match = right_rex.search(text)
+
         return text
+
 
     def load_timings(self):
         return json.loads(zlib.decompress(self.timings))
